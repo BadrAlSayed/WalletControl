c0740fea09b515e56b47196a4c6b03d8fb0b86bf
Badr AlSayed <badrmohamed1000@gmail.com>
2023-09-01 11:54:42 +0300

    feat: update balance of user's wallet in backend

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const User = require("../models/user");
 
 const getUsers = async (req, res) => {
@@ -20,4 +21,20 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { getUsers, createUser };
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { balance } = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No user with id: ${id}`);
+  const currUser = await User.findById(id);
+  currUser.transactions.push({ balance: currUser.balance, date: Date.now() });
+  currUser.balance = balance;
+  try {
+    await currUser.save();
+    res.status(200).json(currUser);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+module.exports = { getUsers, createUser, updateUser };
